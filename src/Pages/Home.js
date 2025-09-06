61b5f4fd499f90b098eb19892246d415def962a0
demo <ayaazq786@gmail.com>
2025-09-06 11:48:52 +0530

    Added home page

---
+++ src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = ({isLoggedIn}) => {
+  return (
+    <div className=' w-full h-full flex justify-center items-center text-gray-200 text-3xl'>
+        Home
+    </div>
+  )
+}
+
+export default Home

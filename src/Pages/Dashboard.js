530c17884d04c3eeeba10e6c9897d6a749932897
demo <ayaazq786@gmail.com>
2025-09-06 11:47:36 +0530

    Added Dashboard page

---
+++ src/Pages/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className=' w-full h-full flex justify-center items-center text-gray-200 text-3xl'>Welcome to studynotion</div>
+  )
+}
+
+export default Dashboard
